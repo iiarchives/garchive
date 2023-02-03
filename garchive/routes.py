450889e84dc270c58b622c122598ebf5bcc6f522
iiPython <ben@iipython.cf>
2023-02-02 21:08:26 -0600

    Adjust a few route paths

--- garchive/routes.py
+++ garchive/routes.py
@@ -16,32 +16,30 @@ async def route_home() -> None:
 
 @app.route("/seasons")
 async def route_seasons() -> None:
-    # seasons = []
-    # for s in os.listdir(season_folder):
-    #     with open(os.path.join(season_folder, season), "r") as fh:
-    #         data = json.loads(fh.read())
+    seasons = []
+    for s in os.listdir(season_folder):
+        with open(os.path.join(season_folder, s, "season.json"), "r") as fh:
+            data = json.loads(fh.read())
 
-    #     data["id"] = s.removesuffix(".json")
-    #     seasons.append(data)
+        data["id"] = s.removesuffix(".json")
+        seasons.append(data)
 
-    # return await view(
-    #     "seasons",
-    #     {"seasons": sorted(seasons, key = lambda s: s["id"], reverse = True)}
-    # )
-    return await view("seasons", {}) # plz help - dmmd
+    return await view(
+        "seasons",
+        {"seasons": sorted(seasons, key = lambda s: s["id"], reverse = True)}
+    )
 
 @app.route("/seasons/{sid}")
 async def route_details(sid: str) -> None:
-    # season_path = os.path.join(season_folder, f"{sid}.json")
-    # if not os.path.isfile(season_path):
-    #     return not_found("No such season exists.")
+    season_path = os.path.join(season_folder, sid, "season.json")
+    if not os.path.isfile(season_path):
+        return not_found("No such season exists.")
 
-    # # Load season information
-    # with open(season_path, "r") as fh:
-    #     data = json.loads(fh.read())
+    # Load season information
+    with open(season_path, "r") as fh:
+        data = json.loads(fh.read())
 
-    # return await view(
-    #     "details",
-    #     {"data": data}
-    # )
-    return await view("details", {}) #also plz help - dmmd
+    return await view(
+        "details",
+        {"data": data}
+    )
